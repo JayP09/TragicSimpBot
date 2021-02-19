fdfd31b6831ec09201cc65c85490f52853697012
Jay Panchal <panchalj272@gmail.com>
2021-02-19 17:32:16 +0530

    added code to avoid error

--- meme.py
+++ meme.py
@@ -2,21 +2,23 @@ import pandas as pd
 import random
 import meme_creator
 
-meme_list = pd.read_csv('discfactbot/memeofweek.csv')
+meme_list = pd.read_csv('memeofweek.csv')
 
 
 def send_meme(rows):
     row_number = random.randint(0, rows)
-    number, page, title, url, count = meme_list.loc[row_number]
+    page, title, url = meme_list.loc[row_number]
     return page, title, url, row_number
-
 
 def meme_main():
     rows, column = meme_list.shape
-    if rows == 0:
+    if rows == 1:
+        page, title, url, row_number = send_meme(rows)
+        meme_list.drop(row_number, inplace=True)
+        meme_list.to_csv('memeofweek.csv', index=False, sep=',')
         meme_creator.meme_file_creator()
     else:
         page, title, url, row_number = send_meme(rows)
         meme_list.drop(row_number, inplace=True)
-        meme_list.to_csv('discfactbot/memeofweek.csv', index=False, sep=',')
+        meme_list.to_csv('memeofweek.csv', index=False, sep=',')
         return page, title, url
