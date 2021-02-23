5645f92e993561508582af8dba0c0c27d4a04f05
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-02-23 11:55:09 +0530

    motivation.py

--- motivation.py
+++ motivation.py
@@ -12,30 +12,31 @@ collection = db["quotes"]
 
 CATEGORY = ['motivation', 'inspiration', 'inspire', 'motivational', 'productive']
 ACCESS_KEY_QUOTES = '96e7fd9bbc2a1bc1d2f8144ef0dbb488'
-def update_db(post):
-    quote = post['quote']
-    author = post['author']
+
+def update_db(quote,author,oneCat,id_value):    
     status = collection.find_one({'quote':quote})
     if status is None:
+        post = {"_id":id_value+1,'quote':quote,'author':author,'Category':oneCat,'up':0 ,'down':0 }
         collection.insert_one(post)
         return quote , author
     else:
+        collection.update_one({ "_id" : status['_id'] },{ "$set" : { "count":status['count']+1 } })
         return quote ,author
+
 def quotes_fav(oneCat=None):
     if oneCat == None:
         oneCat = random.choice(CATEGORY)
         quotes_fav(oneCat)
-    else:
-        if oneCat in [CATEGORY]:
-            id_value = collection.estimated_document_count()
-            response = requests.get('https://favqs.com/api/quotes/',
+    if oneCat in CATEGORY:
+        id_value = collection.estimated_document_count()
+        response = requests.get('https://favqs.com/api/quotes/',
                                         params={'filter':oneCat},
                                         headers={'Authorization':'Token token='+ ACCESS_KEY_QUOTES},verify=False)
-            quote_list_json = response.json()
-            quote = quote_list_json['quotes'][0]['body']
-            author = quote_list_json['quotes'][0]['author']
-            post = {"_id":id_value+1,'quote':quote,'author':author,'Category':oneCat,'up':0 ,'down':0}
-            quote , author = update_db(post)
-            return quote , author
-        else:
-            return 'failed' , 'invalid category'
+        quote_list_json = response.json()
+        quote = quote_list_json['quotes'][0]['body']
+        author = quote_list_json['quotes'][0]['author']
+        
+        quote , author = update_db(quote,author,oneCat,id_value)
+        return quote , author
+    else:
+        return 'failed' , 'invalid category'
