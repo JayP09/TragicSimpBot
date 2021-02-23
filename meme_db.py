4d6ca60dc4a81e5efb0257e7de8113f8174f7271
Jay Panchal <panchalj272@gmail.com>
2021-02-23 20:05:38 +0530

    updated file

--- meme_db.py
+++ meme_db.py
@@ -9,7 +9,7 @@ max_count = list(collection.find().sort("count",-1).limit(1))[0]['count'] #get m
 def send_meme(x=0):
     total_memes = collection.estimated_document_count() #total_meme
     random_number = random.randint(1,total_memes) #random meme by id 
-    meme_data = list(collection.find_one({"_id":random_number}))#cursor object to list 
+    meme_data = collection.find_one({"_id":random_number})#cursor object to list
     if x==0:
         if meme_data['count'] == max_count:
             send_meme(x=1) # just increase the probality of not geting same meme by 1/(totalmeme)**2
